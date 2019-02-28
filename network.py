68c2add677083219f0e0da81f058486db37db8a7
PaParaZz1 <niuyazhe@buaa.edu.cn>
2019-02-28 21:57:28 +0800

    add decoder

--- network.py
+++ network.py
@@ -96,6 +96,53 @@ class ConetentEncoder(nn.Module):
         return x
 
 
+class Decoder(nn.Module):
+    def __init__(self, out_channels, res_num, init_type="xavier", activation=nn.ReLU()):
+        super(Decoder, self).__init__()
+        self.channel_list = [256, 128, 64]
+        self.layers = []
+        for i in range(res_num):
+            self.layers += [M.ResidualBlock(
+                in_channels=self.channel_list[0],
+                out_channels=self.channel_list[0],
+                kernel_size=3,
+                stride=1,
+                padding=1,
+                init_type=init_type,
+                activation=activation,
+                norm_type='IN',
+                is_bottleneck=False
+                )]
+        for i in range(len(self.channel_list) - 1):
+            self.layers += [nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)]
+            self.layers += M.conv2d_block(
+                    in_channels=self.channel_list[i],
+                    out_channels=self.channel_list[i+1],
+                    kernel_size=3,
+                    stride=1,
+                    padding=1,
+                    init_type=init_type,
+                    activation=activation,
+                    norm_type='LN'
+                    )
+
+        self.layers += [M.conv2d_block(
+            in_channels=self.channel_list[-1],
+            out_channels=out_channels,
+            kernel_size=7,
+            stride=1,
+            padding=3,
+            init_type=init_type,
+            activation=nn.Tanh(),
+            norm_type=None
+            )]
+        self.main = nn.Sequential(*self.layers)
+
+    def forward(self, x):
+        x = self.main(x)
+        return x
+
+
 class CariStyleGAN(nn.Module):
     def __init__(self, in_channels, init_type="xavier", norm_type='IN'):
         super(CariStyleGAN, self).__init__()
@@ -111,5 +158,11 @@ def unit_test_encoder():
     print(content_encoder)
 
 
+def unit_test_decoder():
+    decoder = Decoder(out_channels=3, res_num=2)
+    print(decoder)
+
+
 if __name__ == "__main__":
-    unit_test_encoder()
+    # unit_test_encoder()
+    unit_test_decoder()
--- nn_module.py
+++ nn_module.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 from torch.nn.init import xavier_normal_, kaiming_normal_, orthogonal_
 
@@ -26,6 +27,35 @@ def weight_init_(weight, init_type, activation=None):
         init_type_dict[init_type](weight, activation)
     else:
         raise ValueError("Invalid Value in init type: {}".format(init_type))
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, num_features, eps=1e-5, affine=True):
+        super(LayerNorm, self).__init__()
+        self.num_features = num_features
+        self.affine = affine
+        self.eps = eps
+
+        if self.affine:
+            self.gamma = nn.Parameter(torch.Tensor(num_features).uniform_())
+            self.beta = nn.Parameter(torch.zeros(num_features))
+
+    def forward(self, x):
+        shape = [-1] + [1] * (x.dim() - 1)
+        # print(x.size())
+        if x.size(0) == 1:  # batch_size = 1
+            mean = x.view(-1).mean().view(*shape)
+            std = x.view(-1).std().view(*shape)
+        else:
+            mean = x.view(x.size(0), -1).mean(1).view(*shape)
+            std = x.view(x.size(0), -1).std(1).view(*shape)
+
+        x = (x - mean) / (std + self.eps)
+
+        if self.affine:
+            shape = [1, -1] + [1] * (x.dim() - 2)
+            x = x * self.gamma.view(*shape) + self.beta.view(*shape)
+        return x
 
 
 def sequential_pack(layers):
@@ -100,6 +130,8 @@ def conv2d_block(in_channels,
         block.append(nn.BatchNorm2d(out_channels))
     elif norm_type == 'IN':
         block.append(nn.InstanceNorm2d(out_channels, affine=True))
+    elif norm_type == 'LN':
+        block.append(LayerNorm(out_channels, affine=True))
     else:
         raise ValueError
     if activation is not None:
@@ -166,6 +198,8 @@ def deconv2d_block(in_channels,
         block.append(nn.BatchNorm2d(out_channels))
     elif norm_type == 'IN':
         block.append(nn.InstanceNorm2d(out_channels, affine=True))
+    elif norm_type == 'LN':
+        block.append(LayerNorm(out_channels, affine=True))
     else:
         raise ValueError
     if activation is not None:
